b1e6177c69aba3fa27af7137c7966ffe3d4ccc58
Bokinhkong <bokinhkong@gmail.com>
2018-08-09 07:17:19 +0700

    Mongoose

--- api/controllers/category.controller.js
+++ api/controllers/category.controller.js
@@ -3,93 +3,35 @@ const mongoose = require("mongoose");
 const Category = require("../models/category.model");
 
 exports.categories_get_all = (req, res, next) => {
-  Category.find()    
-    .exec()
-    .then(docs => {
-      const response = docs.map(doc => {
-        return {
-          id: doc._id,
-          name: doc.name
-        };
-      });
-
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+  Category.find((err, docs) => {
+    if (err) return res.status(500).send({error: err});
+    return res.status(200).send(docs);   
+  });    
 };
 
-exports.categories_create = (req, res, next) => {  
-  const category = new Category({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name
+exports.categories_create = (req, res, next) => {     
+  const category = new Category(req.body);
+  category.id = new mongoose.Types.ObjectId();
+
+  category.save(err => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(category);
+  });  
+};
+
+exports.categories_get_by_id = (req, res, next) => {  
+  Category.findById(req.params.id, (err, doc) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(doc);
   });
-  category
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created Category successfully",
-        createdCategory: {            
-            id: result._id,
-            name: result.name
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-};
-
-exports.categories_get_by_id = (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  Category.findById(id)
-    .select('name price _id')
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json({            
-          id: doc._id,
-          name: doc.name
-        });
-      } else {
-        res.status(404).json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
 };
 
 exports.categories_get_by_name = (req, res, next) => {
-  const name = req.query.name;
-  console.log(name);
-  Category.find({"name": name})
-    .select('name price _id')
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json({            
-          id: doc._id,
-          name: doc.name
-        });
-      } else {
-        res.status(404).json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
+  const name = req.query.name;  
+  Category.findOne({name: name}, (err, doc) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(doc);
+  });
 };
 
 exports.categories_update = (req, res, next) => {
@@ -97,45 +39,18 @@ exports.categories_update = (req, res, next) => {
     const error = new Error('Bad request');
     error.status = 400;
     next(error);    
-  }
-
-  const category = new Category({
-    _id: req.body.id,
-    name: req.body.name
+  }  
+  
+  Category.findByIdAndUpdate(req.params.id, req.body, (err, doc) => {
+    if (err) return res.status(500).send(err);
+    res.status(200).send(req.body);
   });
-  category.update()
-    .exec()
-    .then(result => {
-      res.status(200).json({
-          message: 'Category updated',
-          createdCategory: {            
-            id: result._id,
-            name: result.name
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
 };
 
-exports.categories_delete = (req, res, next) => {
-  const id = req.params.id;
-  Category.remove({ _id: id })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-          message: 'Category deleted'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+exports.categories_delete = (req, res, next) => {  
+  Category.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (err) res.status(500).send(err);
+    res.status(200).send(doc);
+  });
 };
 
--- api/models/category.model.js
+++ api/models/category.model.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-    id: mongoose.Schema.Types.ObjectId,
+    _id: {type: mongoose.Schema.Types.ObjectId, alias: 'id'},
     name: { type: String, required: true }
 });
+
+if (!categorySchema.options.toObject) {
+    categorySchema.options.toObject = {};
+}
+  
+categorySchema.options.toObject.transform =  function(doc, ret) {
+    // Set the id from the retrun object value which will be a string.
+    ret.id = ret._id;
+
+    delete ret._id;
+    delete ret.__v;
+};
+  
 
 module.exports = mongoose.model('Category', categorySchema);
