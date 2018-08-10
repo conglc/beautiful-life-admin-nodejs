f7fc876c1051ed4ac429616a82e3889a5396cdac
Bokinhkong <bokinhkong@gmail.com>
2018-08-10 07:43:42 +0700

    article api

--- api/controllers/article.controller.js
+++ api/controllers/article.controller.js
@@ -4,88 +4,30 @@ const Article = require("../models/article.model");
 
 
 exports.articles_get_all = (req, res, next) => {
-  Article.find()    
-    .exec()
-    .then(docs => {
-      const response = docs.map(doc => {
-        return {
-          id: doc._id,
-          title: doc.title,
-          description: doc.description,
-          content: doc.content,
-          image: doc.image,
-          createdAt: doc.createdAt,
-          categoryId: doc.categoryId,
-        };
-      });
-
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+  Article.find((err, docs) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(docs);
+  });
 };
 
 exports.articles_create = (req, res, next) => {  
-  const article = new Article({
-    _id: new mongoose.Types.ObjectId(),
-    title: req.body.title,
-    description: req.body.description,
-    content: req.body.content,
-    image: req.body.image,
-    createdAt: req.body.createdAt,
-    categoryId: req.body.categoryId,
+  const article = new Article(req.body);
+  article.id = new mongoose.Types.ObjectId();
+
+  article.save(err => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(article);
   });
-  article
-    .save()
-    .then(doc => {      
-      res.status(201).json({
-        message: "Created Article successfully",
-        createdArticle: {            
-          id: doc._id,
-          title: doc.title,
-          description: doc.description,
-          content: doc.content,
-          image: doc.image,
-          createdAt: doc.createdAt,
-          categoryId: doc.categoryId,
-        }
-      });
-    })
-    .catch(err => {      
-      res.status(500).json({
-        error: err
-      });
-    });
 };
 
 exports.articles_get_by_id = (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  Article.findById(id)    
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json({            
-          id: doc._id,
-          title: doc.title,
-          description: doc.description,
-          content: doc.content,
-          image: doc.image,
-          createdAt: doc.createdAt,
-          categoryId: doc.categoryId,
-        });
-      } else {
-        res.status(404).json({ message: "No valid entry found for provided ID" });
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
+  Article.findById(req.params.id, (err, doc) => {
+    if (err) return res.status(500).send(err);
+
+    if (doc == null) return res.status(404).send({"message": "Not found"});
+
+    if (doc) return res.status(200).send(doc);
+  })    
 };
 
 exports.articles_update = (req, res, next) => {
@@ -95,53 +37,17 @@ exports.articles_update = (req, res, next) => {
     next(error);    
   }
 
-  const article = new Article({
-    _id: req.body.id,    
-    title: req.body.title,
-    description: req.body.description,
-    content: req.body.content,
-    image: req.body.image,
-    createdAt: req.body.createdAt,
-    categoryId: req.body.categoryId,
+  const article = new Article(req.body);
+  Article.findByIdAndUpdate(req.params.id, article, (err, doc) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(article);
   });
-  Article.findByIdAndUpdate(req.params.id, article)
-    .exec()
-    .then(doc => {
-      res.status(200).json({
-          message: 'Article updated',
-          createdArticle: {            
-            id: doc._id,
-            title: doc.title,
-            description: doc.description,
-            content: doc.content,
-            image: doc.image,
-            createdAt: doc.createdAt,
-            categoryId: doc.categoryId,
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
 };
 
 exports.articles_delete = (req, res, next) => {
-  const id = req.params.id;
-  Article.remove({ _id: id })
-    .exec()
-    .then(doc => {
-      res.status(200).json({
-          message: 'Article deleted'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
+  Article.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(doc);    
+  });
 };
 
--- api/models/article.model.js
+++ api/models/article.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const articleSchema = mongoose.Schema({
-    id: mongoose.Schema.Types.ObjectId,
+    _id: {type: mongoose.Schema.Types.ObjectId, alias: 'id'},
     title: { type: String, required: true },
     description: { type: String },
     content: { type: String },
@@ -9,5 +9,17 @@ const articleSchema = mongoose.Schema({
     createdAt: { type: Date },
     categoryId: { type: String, required: true },
 });
+
+if (!articleSchema.options.toObject) {
+    articleSchema.options.toObject = {};
+}
+  
+articleSchema.options.toObject.transform =  function(doc, ret) {
+    // Set the id from the retrun object value which will be a string.
+    ret.id = ret._id;
+
+    delete ret._id;
+    delete ret.__v;
+};
 
 module.exports = mongoose.model('Article', articleSchema);
